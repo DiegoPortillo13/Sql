c40486038038afd48fd8fcafb52ef29655fb75a6
DiegoPortillo13 <31285973+DiegoPortillo13@users.noreply.github.com>
2018-05-02 20:28:03 -0600

    Add files via upload

---
+++ banbajioBDDrespaldoPorSi.sql
@@ -0,0 +1,161 @@
+SELECT * from cliente;
+
+select *
+from tel_cliente;
+/*insert into tel_cliente(no_cliente, telefono)
+values (16550474, '6142496957');*/
+
+select *
+from tel_cliente;
+
+select *
+from cliente, tel_cliente
+where cliente.no_cliente = tel_cliente.no_cliente; 
+
+select sum(saldo) as totalbanco 
+from cuenta, cliente
+where cuenta.cliente_no_cliente = cliente.no_cliente
+order by cliente.a_paterno;
+
+select *
+from cuenta c, sucursal s
+where c.sucursal_no_sucursal = s.no_sucursal;
+
+select s.nombre, count(*)
+from cuenta c, sucursal s
+where c.sucursal_no_sucursal = s.no_sucursal
+group by s.no_sucursal;
+
+select s.nombre, sum(saldo)
+from cuenta c, sucursal s
+where c.sucursal_no_sucursal = s.no_sucursal
+group by s.no_sucursal;
+
+select *
+from cuenta c, sucursal s, cliente cl
+where c.sucursal_no_sucursal = s.no_sucursal
+and c.cliente_no_cliente = cl.no_cliente;
+
+select * from empleado;
+
+select *
+from empleado e, sucursal s
+where e.sucursal_no_sucursal = s.no_sucursal;
+
+select * from transaccion;
+select * from movimiento;
+
+select *
+from transaccion t, movimiento m
+where t.movimiento_id_movimiento = m.id_movimiento
+and (fecha >= '2018-04-01' and fecha <= '2018-04-30')
+order by t.cuenta_no_cuenta;
+
+select m.descripcion, count(*) as num_movimiento, sum(t.cantidad)
+from transaccion t, movimiento m
+where t.movimiento_id_movimiento = m.id_movimiento
+group by m.id_movimiento;
+
+select * 
+from cliente c, cuenta cn, tipo_cuenta tc, sucursal s
+where c.no_cliente = cn.cliente_no_cliente
+and cn.tipo_cuenta_no_tipo = tc.no_tipo
+and cn.sucursal_no_sucursal = s.no_sucursal;
+
+select c.no_cliente, c.nombre, c.a_paterno, cn.no_cuenta, cn.saldo, tc.descripcion, s.nombre
+from cliente c, cuenta cn, tipo_cuenta tc, sucursal s
+where c.no_cliente = cn.cliente_no_cliente
+and cn.tipo_cuenta_no_tipo = tc.no_tipo
+and cn.sucursal_no_sucursal = s.no_sucursal 
+group by s.no_sucursal;
+
+select s.nombre, count(*) as num_cuentas, sum(cn.Saldo)
+from cliente c, cuenta cn, tipo_cuenta tc, sucursal s
+where c.no_cliente = cn.cliente_no_cliente
+and cn.tipo_cuenta_no_tipo = tc.no_tipo
+and cn.sucursal_no_sucursal = s.no_sucursal 
+group by s.no_sucursal;
+
+insert into transaccion (no_transaccion, cantidad, fecha, movimiento_id_movimiento, cuenta_no_cuenta)
+values (19, 45000, '2018-05-21', 2, 16550474);
+
+select*
+from cliente
+where a_paterno like 'm%';
+
+update cliente
+set nombre=upper(nombre),a_paterno=upper(a_paterno),a_materno=upper(a_materno);
+
+select*
+from transaccion t, cuenta cn, movimiento m,
+tipo_cuenta tp, sucursal s
+where t.cuenta_no_cuenta=cn.no_cuenta
+ and t.movimiento_id_movimiento=m.id_movimiento
+  and cn.tipo_cuenta_no_tipo=tp.no_tipo
+   and cn.sucursal_no_sucursal=s.no_sucursal;
+
+select s.nombre,m.descripcion,sum(t.cantidad)
+from transaccion t, cuenta cn, movimiento m,
+tipo_cuenta tp, sucursal s
+where t.cuenta_no_cuenta=cn.no_cuenta
+ and t.movimiento_id_movimiento=m.id_movimiento
+  and cn.tipo_cuenta_no_tipo=tp.no_tipo
+   and cn.sucursal_no_sucursal=s.no_sucursal
+group by s.no_sucursal,m.id_movimiento ;
+/*1.-*/
+select c.nombre, cn.tipo_cuenta_no_tipo,tp.descripcion
+from cuenta cn, tipo_cuenta tp, cliente c
+where cn.tipo_cuenta_no_tipo=tp.no_tipo
+	and cn.cliente_no_cliente=c.no_cliente
+order by c.nombre;
+/*2.-*/
+select s.nombre, e.nombre
+from sucursal s, empleado e
+where e.sucursal_no_sucursal=s.no_sucursal
+order by s.no_sucursal;
+
+/*3.-*/
+select m.descripcion, sum(t.cantidad)
+from movimiento m, transaccion t
+where t.movimiento_id_movimiento=m.id_movimiento
+group by m.descripcion;
+
+/*4.-*/
+select s.nombre, count(m.id_movimiento)
+from movimiento m, sucursal s,cuenta cn, transaccion t
+where cn.sucursal_no_sucursal=s.no_sucursal
+	and t.cuenta_no_cuenta=cn.no_cuenta
+		and t.movimiento_id_movimiento=m.id_movimiento
+group by s.nombre;
+
+/*5.-*/
+select c.nombre,c.a_paterno,c.a_materno,count(m.id_movimiento)
+from cuenta cn, cliente c, transaccion t, movimiento m
+where t.movimiento_id_movimiento=m.id_movimiento
+	and t.cuenta_no_cuenta=cn.no_cuenta
+		and cn.cliente_no_cliente=c.no_cliente
+group by c.nombre;
+
+/*6.-*/
+select tc.descripcion, count(m.id_movimiento)
+from tipo_cuenta tc, movimiento m,transaccion t,cuenta cn
+where cn.tipo_cuenta_no_tipo=tc.no_tipo
+	and t.movimiento_id_movimiento=m.id_movimiento
+		and t.cuenta_no_cuenta=cn.no_cuenta
+group by tc.no_tipo;
+
+/*7.-*/
+select s.nombre, count(cn.no_cuenta)
+from cuenta cn, sucursal s
+where cn.sucursal_no_sucursal=s.no_sucursal
+group by s.nombre;
+
+/*8.-*/
+select c.nombre,c.a_paterno,c.a_materno, max(cn.saldo)
+from cliente c, cuenta cn
+where cn.cliente_no_cliente=c.no_cliente
+order by cn.saldo;
+
+
+
+
